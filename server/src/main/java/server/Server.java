0b588a8fb1ed79095645081f5aa88e260c0aeaa4
driggstyler <driggstyler@gmail.com>
2024-08-06 11:53:31 -0600

    Restored parts for Phase 3/4, fixed dependencies, "no driver for sql found" and "too many connections".

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import Handlers.*;
 import spark.*;
 
 public class Server {
@@ -10,9 +11,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.delete("/db", (req, res) -> new ClearHandler().handle(req, res));
+        Spark.post("/game", (req, res) -> new CreateGameHandler().handle(req, res));
+        Spark.put("/game", (req, res) -> new JoinGameHandler().handle(req, res));
+        Spark.get("/game", (req, res) -> new ListGamesHandler().handle(req, res));
+        Spark.post("/session", (req, res) -> new LoginHandler().handle(req, res));
+        Spark.delete("/session", (req, res) -> new LogoutHandler().handle(req, res));
+        Spark.post("/user", (req, res) -> new RegisterHandler().handle(req, res));
 
         Spark.awaitInitialization();
         return Spark.port();
